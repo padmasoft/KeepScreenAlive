f3f6f512fcc0858dbfbd587de667d4bf7ec354ea
padmasoft <98417964+padmasoft@users.noreply.github.com>
2022-01-25 23:20:28 +0530

    Initial commit

---
+++ HomeForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace KeepScreenAwake
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeForm));
+            this.toggle_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // toggle_button
+            // 
+            this.toggle_button.Location = new System.Drawing.Point(12, 12);
+            this.toggle_button.Name = "toggle_button";
+            this.toggle_button.Size = new System.Drawing.Size(75, 23);
+            this.toggle_button.TabIndex = 0;
+            this.toggle_button.UseVisualStyleBackColor = true;
+            this.toggle_button.Click += new System.EventHandler(this.toggle_button_Click);
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 48);
+            this.Controls.Add(this.toggle_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "HomeForm";
+            this.Text = "Keep Screen Awake";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button toggle_button;
+    }
+}
+
---
+++ HomeForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeepScreenAwake
+{
+    public partial class HomeForm : Form
+    {
+        private bool buttonClicked = false;
+        public bool ButtonClicked
+        {
+            get
+            {
+                return buttonClicked;
+            }
+            set
+            {
+                buttonClicked = value;
+                if (buttonClicked)
+                {
+                    toggle_button.Text = "End";
+                }
+                else
+                {
+                    toggle_button.Text = "Start";
+                }
+            }
+        }
+
+        public HomeForm()
+        {
+            InitializeComponent();
+            toggle_button.Text = "Start";
+        }
+
+        private void toggle_button_Click(object sender, EventArgs e)
+        {
+            ButtonClicked = !ButtonClicked;
+            _ = DancingCursorLogicAsync();
+        }
+
+        private async Task DancingCursorLogicAsync()
+        {
+            try
+            {
+                do
+                {
+                    if (buttonClicked)
+                    {
+                        MoveCursor(Cursor.Position.X - 5, Cursor.Position.Y - 5);
+                        await Task.Delay(2000);
+                    }
+
+                    if (buttonClicked)
+                    {
+                        MoveCursor(Cursor.Position.X + 5, Cursor.Position.Y + 5);
+                        await Task.Delay(2000);
+                    }
+
+                } while (buttonClicked);
+            }
+            finally
+            {
+            }
+        }
+
+        private void MoveCursor(int x, int y)
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(x, y);
+            Cursor.Clip = new Rectangle(this.Location, this.Size);
+        }
+
+    }
+}
