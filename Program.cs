3fc54a0f1654901d8ae30d780045bd37f393b1e6
padmasoft <98417964+padmasoft@users.noreply.github.com>
2022-02-26 16:18:02 +0530

    test

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace KeepScreenAwake
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new HomeForm());
+            //done
         }
     }
 }
